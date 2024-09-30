40c1a81b521d0555d26f341e091cbe352b742a49
Shreyas Kothari <spkothari0@gmail.com>
2024-09-29 21:05:31 -0400

    global exception update

--- CloudDemo_CSYE_6225/src/main/java/com/shreyas/CloudDemo/exceptions/GlobalExceptionHandler.java
+++ CloudDemo_CSYE_6225/src/main/java/com/shreyas/CloudDemo/exceptions/GlobalExceptionHandler.java
@@ -1,17 +1,18 @@
 package com.shreyas.CloudDemo.exceptions;
 
 import com.shreyas.CloudDemo.controller.BaseController;
+import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException;
 
 import java.sql.SQLException;
 
@@ -20,50 +21,56 @@ import java.sql.SQLException;
 public class GlobalExceptionHandler extends BaseController {
 
     @ExceptionHandler(SQLException.class)
-    public ResponseEntity<Void> handleSQLException(SQLException ex) {
-        log.error("SQLException {}",ex.getMessage());
-        return ErrorResponse(HttpStatus.SERVICE_UNAVAILABLE);
+    public ResponseEntity<Object> handleSQLException(SQLException ex) {
+        log.error("SQLException {}", ex.getMessage());
+        return ErrorResponse(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage());
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<Void> handleIllegalArgumentException(AuthenticationException ex) {
-        log.error("AuthenticationException {}",ex.getMessage());
-        return ErrorResponse(HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Object> handleIllegalArgumentException(AuthenticationException ex) {
+        log.error("AuthenticationException {}", ex.getMessage());
+        return ErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
 
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<Void> handleBadCredentialException(AuthenticationException ex) {
-        log.error("BadCredentialsException {}",ex.getMessage());
-        return ErrorResponse(HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Object> handleBadCredentialException(AuthenticationException ex) {
+        log.error("BadCredentialsException {}", ex.getMessage());
+        return ErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
 
-    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
-    public ResponseEntity<Void> handleUnauthorizedException(HttpClientErrorException.Unauthorized ex) {
-        log.error("Unauthorized Exception {}",ex.getMessage());
-        return ErrorResponse(HttpStatus.FORBIDDEN);
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> handleUnauthorizedException(AccessDeniedException ex) {
+        log.error("Unauthorized Exception {}", ex.getMessage());
+        return ErrorResponse(HttpStatus.FORBIDDEN, ex.getMessage());
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<Void> handleUsernameNotFoundException(UsernameNotFoundException ex) {
-        log.error("UsernameNotFoundException {}",ex.getMessage());
-        return ErrorResponse(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        log.error("UsernameNotFoundException {}", ex.getMessage());
+        return ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Void> handleException(Exception ex) {
-        log.error("Unexpected Exception Occurred {}",ex.getMessage());
+    public ResponseEntity<Object> handleException(Exception ex) {
+        log.error("Unexpected Exception Occurred {}", ex.getMessage());
         return ExceptionResponse(ex);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<Void> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
-        log.error("HttpRequestMethodNotSupportedException {}",ex.getMessage());
-        return ErrorResponse(HttpStatus.METHOD_NOT_ALLOWED);
+    public ResponseEntity<Object> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+        log.error("HttpRequestMethodNotSupportedException {}", ex.getMessage());
+        return ErrorResponse(HttpStatus.METHOD_NOT_ALLOWED, ex.getMessage());
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<Void> handleBadRequestException(BadRequestException ex) {
-        log.error("BadRequestException {}",ex.getMessage());
-        return ErrorResponse(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
+        log.error("BadRequestException {}", ex.getMessage());
+        return ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Object> handleValidationException(ValidationException ex) {
+        log.error("ValidationException {}", ex.getMessage());
+        return ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 }
