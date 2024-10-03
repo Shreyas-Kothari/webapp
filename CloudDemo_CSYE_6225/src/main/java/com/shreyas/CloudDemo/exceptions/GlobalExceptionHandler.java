e5ecfcb60f0254b8e8784ab02825662ae429702c
Shreyas Kothari <spkothari0@gmail.com>
2024-10-02 23:08:48 -0400

    refactor

--- CloudDemo_CSYE_6225/src/main/java/com/shreyas/CloudDemo/exceptions/GlobalExceptionHandler.java
+++ CloudDemo_CSYE_6225/src/main/java/com/shreyas/CloudDemo/exceptions/GlobalExceptionHandler.java
@@ -14,7 +14,6 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.sql.SQLException;
