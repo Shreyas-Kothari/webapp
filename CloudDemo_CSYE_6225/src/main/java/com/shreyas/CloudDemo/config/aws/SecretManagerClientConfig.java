c7203419e7416824d9e2ea9a99617f2c9d1b3fa3
Shreyas Kothari <spkothari0@gmail.com>
2024-11-30 10:54:20 -0500

    uncommenting instance profile code

--- CloudDemo_CSYE_6225/src/main/java/com/shreyas/CloudDemo/config/aws/SecretManagerClientConfig.java
+++ CloudDemo_CSYE_6225/src/main/java/com/shreyas/CloudDemo/config/aws/SecretManagerClientConfig.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-import software.amazon.awssdk.services.sns.SnsClient;
 
 @Configuration
 public class SecretManagerClientConfig {
@@ -19,7 +19,7 @@ public class SecretManagerClientConfig {
         return SecretsManagerClient.builder()
                 .region(Region.of(s3Region))
                 // comment below line to run in local
-//                .credentialsProvider(InstanceProfileCredentialsProvider.create())
+                .credentialsProvider(InstanceProfileCredentialsProvider.create())
                 .build();
     }
 }
